126c19c33e406dd7dfd081fd6aaf3db4baf81ff2
Liwaa Mazen Mezher <usbminnet@hotmail.com>
2020-08-28 14:07:22 +0200

    header done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
-
+import Header from './Header';
+import Sidebar from "./Sidebar";
 function App() {
   return (
     // BEM naming convention
     <div className="app">
-      <h1>Lets build the facebook clone!</h1>
+      <Header />
 
-      {/* Header */}
-      
+      <div className="app__body">
+        <Sidebar />
+        {/* Sidebar */}
+        {/* Feed */}
+        {/* Widgets */}
+      </div>
+        
     </div>
   );
 }
