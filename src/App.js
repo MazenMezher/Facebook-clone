c9bb37f81ac9f3da24630bcc19f4d1ca6da2f583
Liwaa Mazen Mezher <usbminnet@hotmail.com>
2020-08-28 20:45:58 +0200

    loggin auth with google and firebase

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from "./Feed";
 import Widgets from "./Widgets";
-
+import Login from './Login';
+import { useStateValue } from "./StateProvider";
 
 function App() {
-  const user = null;
+  const [{ user }, dispatch] = useStateValue();
+
   return (
     // BEM naming convention
     <div className="app">
-      {!user ? <h1>Login</h1> : (
+      {!user ? ( 
+        <Login />
+        ) : (
         <>
         <Header />
         <div className="app__body">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCNba-hZKANxUYO3_JC9mghKjqy3vPLzl8",
     authDomain: "facebook-clone-971ea.firebaseapp.com",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:677255753459:web:6a4227a5693c05627c5186",
     measurementId: "G-CQ0QJTKT0G"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
