e600ca5664787a9d9f17bf80f3dda2df4a74cdce
Liwaa Mazen Mezher <usbminnet@hotmail.com>
2020-08-28 18:50:24 +0200

    styling for where posting messages will go

--- src/Feed.js
+++ src/Feed.js
@@ -2,13 +2,28 @@ import React from 'react'
 import "./Feed.css";
 import StoryReel from "./StoryReel";
 import MessageSender from "./MessageSender";
+import Post from './Post';
 
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
             <MessageSender />
-            {/* MessageSender */}
+            
+            <Post 
+                profilePic="https://images.interactives.dk/shutterstock_245163850-1-axkokcc1_5ggif3skctjcg-DFcygg6HO_y0WdmrlCREJw.jpg?auto=compress&ch=Width%2CDPR&dpr=2.63&h=450&ixjsv=2.2.4&q=38"
+                message="Wow this works"
+                timestamp="Tis is a timestamp"
+                username="Henry"
+                image="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg"
+            />
+            <Post 
+                profilePic="https://images.interactives.dk/shutterstock_245163850-1-axkokcc1_5ggif3skctjcg-DFcygg6HO_y0WdmrlCREJw.jpg?auto=compress&ch=Width%2CDPR&dpr=2.63&h=450&ixjsv=2.2.4&q=38"
+                message="Wow this works"
+                timestamp="Tis is a timestamp"
+                username="Henry"
+            />
+            <Post />
         </div>
     )
 }
--- src/MessageSender.js
+++ src/MessageSender.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import "./MessageSender.css"
 import { Avatar } from '@material-ui/core'
 import VideocamIcon from "@material-ui/icons/Videocam";
@@ -6,9 +6,14 @@ import PhotoLibraryIcon from "@material-ui/icons/PhotoLibrary";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 
 function MessageSender() {
+    const [input, setInput] = useState("");
+    const [imageUrl, setImageUrl] = useState("");
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
+        setInput("");
+        setImageUrl("");
     };
 
     return (
@@ -16,11 +21,18 @@ function MessageSender() {
             <div className="messageSender__top">
                 <Avatar />
                 <form>
-                    <input type="text"
+                    <input 
+                    value={input}
+                    onChange={e => setInput(e.target.value)}
                     className="messageSender__input"
                     placeholder={"What's on your mind?"}
                     />
-                    <input type="text" placeholder={"image URL (Optional"} />
+                    <input
+                    value={imageUrl}
+                    onChange={e => setImageUrl(e.target.value)}
+                    type="text" 
+                    placeholder={"image URL (Optional"} 
+                    />
 
                     <button onClick={handleSubmit} type="submit">
                         Hidden Submit
