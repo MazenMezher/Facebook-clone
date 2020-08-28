e2c3205bfbb9bd401fc9236c7869c2be2d7eaae9
Liwaa Mazen Mezher <usbminnet@hotmail.com>
2020-08-28 10:02:08 +0200

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCNba-hZKANxUYO3_JC9mghKjqy3vPLzl8",
+    authDomain: "facebook-clone-971ea.firebaseapp.com",
+    databaseURL: "https://facebook-clone-971ea.firebaseio.com",
+    projectId: "facebook-clone-971ea",
+    storageBucket: "facebook-clone-971ea.appspot.com",
+    messagingSenderId: "677255753459",
+    appId: "1:677255753459:web:6a4227a5693c05627c5186",
+    measurementId: "G-CQ0QJTKT0G"
+  };
